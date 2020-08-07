1305a6164d7a630a332874fe6ad1781ef7a09e2f
Sarel Micha <Sarelmicha93@gmail.com>
2020-08-07 09:45:32 +0300

    *Moving door has been added

---
+++ Assets/Scripts/DoorMotion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMotion : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Open()
+    {
+        animator.SetBool("isOpen", true);
+    }
+
+    public void Close()
+    {
+        animator.SetBool("isOpen", false);
+    }
+}
---
+++ Assets/Scripts/TriggerDoor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDoor : MonoBehaviour
+{
+
+    [SerializeField] GameObject doorAxis;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+       
+        doorAxis.GetComponent<DoorMotion>().Open();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        doorAxis.GetComponent<DoorMotion>().Close();
+    }
+
+
+}
